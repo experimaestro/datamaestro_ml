08412d90a14986e784d6bca32f8b2f6922715781
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-11-30 12:07:30 +0100

    UCI: Boston

--- datamaestro_ml/config/edu/uci.py
+++ datamaestro_ml/config/edu/uci.py
@@ -1,5 +1,13 @@
-name: Boston Housing
-description: 
+from datamaestro.data import Generic
+from datamaestro.data.csv import Matrix
+from datamaestro.definitions import Dataset
+from datamaestro.download.single import DownloadFile
+
+@DownloadFile("data", url="https://raw.githubusercontent.com/scikit-learn/scikit-learn/master/sklearn/datasets/data/boston_house_prices.csv")
+@Dataset(Generic)
+def boston(data):
+  """Boston Housing dataset
+  
   This dataset contains information collected by the U.S Census Service
   concerning housing in the area of Boston Mass. It was obtained from the
   StatLib archive (http://lib.stat.cmu.edu/datasets/boston), and has been used
@@ -8,12 +16,8 @@ description:
   suspect. The dataset is small in size with only 506 cases. The data was
   originally published by Harrison, D. and Rubinfeld, D.L. `Hedonic prices and
   the demand for clean air', J. Environ. Economics & Management, vol.5, 81-102,
-  1978. 
-
-download: !@/single:File
-  url: https://raw.githubusercontent.com/scikit-learn/scikit-learn/master/sklearn/datasets/data/boston_house_prices.csv
-  type: !@/csv:Matrix
-    names-row: 0
-    ignore: 1
-    target: MEDV
+  1978. """
+  return {
+    "data": Matrix(path=data.path, names_row=0, ignore=1, target="MEDV")
+  }
   
