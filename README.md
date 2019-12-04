526fb1e5888fc1a6e759a615feb1b2fd1b51e9d2
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-12-04 17:53:30 +0100

    Version 2019.12.4

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Text-related datasets
 
-This [datamaestro](https://github.com/experimaestro/datamaestro) plugin covers image-related datasets.
+This [datamaestro](https://github.com/bpiwowar/datasets) plugin covers machine-learning related datasets (general purpose). 
 
+## List of available datasets
 
-The list of available datasets and usage instruction can be found in the [documentation](https://experimaestro.github.io/datamaestro_image/).
+Below is the list of available datasets along with ids. Some datasets have several versions; in this case, the dataset id is suffixed with this information.
+
+- Boston housing (UCI)
--- setup.py
+++ setup.py
@@ -1,29 +1,51 @@
+import os
+from pathlib import Path
+import re
 import sys
 try:
     from setuptools import setup, find_namespace_packages
 except ImportError:
     print("Please upgrade pip: find_namespace_packages not found")
     sys.exit(1)
+from setuptools.command.install import install
 
 # Date-based versioning
-VERSION='2019.12.04'
+VERSION='2019.12.4'
+
+RE_BLANCK=re.compile(r"^\s*(#.*)?$")
+with (Path(__file__).parent / 'requirements.txt').open() as f:
+    requirements = [x for x in f.read().splitlines() if not RE_BLANCK.match(x)]
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='datamaestro_ml',
     version=VERSION,
-    description='Machine Learning related datasets',
+    description='General purpose machine-learning datasets',
     author='Benjamin Piwowarski',
     author_email='benjamin@piwowarski.fr',
     url='https://github.com/bpiwowar/datamaestro_ml',
     license='MIT',
     python_requires='>=3.5',
     packages=find_namespace_packages(include="datamaestro_ml.*"),
-    package_data={
-        '': [ '*.yaml' ]
-    },
-    install_requires=[
-        'datamaestro>=0.5.1'
-    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    install_requires=requirements,
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
@@ -36,8 +58,11 @@ setup(
     ],
     entry_points={
         'datamaestro.repositories': [
-            'ml = datamaestro_ml:Repository'
+            'text = datamaestro_ml:Repository'
         ]
+    },
+    cmdclass={
+        'verify': VerifyVersionCommand,
     },
 
     test_suite='datamaestro_ml.test'
