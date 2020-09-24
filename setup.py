1eb57c650b08879984622941036d2c9f995ea832
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-09-24 14:13:23 +0200

    updates

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 from setuptools import setup
 
-setup(install_requires=Path("requirements.txt").read_text())
+setup(install_requires=Path("requirements.txt").read_text(), use_scm_version=True)
